f1fd8aad658ea3fd1b99912a61d4c9693233e411
Wade Stevens <wayde22@gmail.com>
2018-10-24 21:48:28 -0500

    first commit

---
+++ github.js
@@ -0,0 +1,23 @@
+class Github {
+    constructor() {
+        this.client_id='6c2546d2260c44aa89fa';
+        this.client_secret = 'f949724f942f624c69c182cd397063830645a3ec';
+        this.repos_count = 25;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
